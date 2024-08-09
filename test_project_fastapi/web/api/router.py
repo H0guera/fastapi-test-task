17d09fbc62ee0fe9a90054a352677506952ddbb3
hoguera <shalyginetc@gmail.com>
2024-08-09 18:13:12 +0300

    Added orders router

--- test_project_fastapi/web/api/router.py
+++ test_project_fastapi/web/api/router.py
@@ -1,5 +1,5 @@
 from fastapi.routing import APIRouter
-from test_project_fastapi.web.api import echo
+from test_project_fastapi.web.api import echo, orders
 from test_project_fastapi.web.api import docs
 from test_project_fastapi.web.api import monitoring
 
@@ -7,3 +7,4 @@ api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
+api_router.include_router(orders.router, prefix="/orders", tags=["order"])
